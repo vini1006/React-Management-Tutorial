2efa819c478a5ada35fe03cbf8df87b12906eecb
DESKTOP-JBIPN4D\work <viniyoon1006@gmail.com>
2021-03-13 13:06:25 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '윤빈',
+  'birthday' : '931006',
+  'gender' : '남자',
+  'job' : '신입'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : '윤송',
+    'birthday' : '931006',
+    'gender' : '남자',
+    'job' : '시니어'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/3',
+    'name' : '윤산',
+    'birthday' : '931006',
+    'gender' : '남자',
+    'job' : '대표'
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>shit hell</h2>
+    <div>
+      {
+        customers.map( c => {
+          return (
+            <Customer
+              key = {c.id}
+              id = {c.id}
+              image = {c.image}
+              name = {c.name}
+              birthday = {c.birthday}
+              gender = {c.gender}
+            />
+          );
+        }) 
+      }
     </div>
   );
 }
